5c2f304ff4744391b9210e1fe1c92359b73abcfc
Leandro Santiago <leandrosansilva@gmail.com>
2011-10-10 17:39:12 -0300

    ops, algumas modificações para novas classes, ainda não existentes

--- social-network/CommandFactory.cs
+++ social-network/CommandFactory.cs
@@ -10,17 +10,38 @@ namespace socialnetwork
 		
 		static public Command create(string commandInput)
 		{
-			string[] splitted = commandInput.Split(' ');
-		
+			// obtenho o comando passado
+			string key = "";
+			int i = 0;
+			for (;i < commandInput.Length && commandInput[i] != ' '; i++) {
+				key += commandInput[i];
+			}
+			
 			// o que foi passado, sem o comando, só os dados
-			string payload = String.Join(" ",splitted,1,splitted.Length - 1);
+			string payload = commandInput.Substring(i).TrimStart();
 			
 			// splitted[0] guarda o comando a ser executado
-			switch (splitted[0]) {
-				case "create-user":
+			switch (key) {
+				case "criar-usuario":
 					return new CreateUserCommand(payload);
-				break;
+				case "listar-mensagens-usuario":
+					return new ListUserMessagesCommand(payload);
+				case "postar-mensagem":
+					return new PostMessageCommand(payload);
+				case "listar-seguidores":
+					return new ListFollowersCommand(payload);
+				case "seguir":
+					return new FollowCommand(payload);
+				case "listar-seguidos":
+					return new ListFollowedCommand(payload);
+				case "deixar-de-seguir":
+					return StopFollowingCommand(payload);
+				case "listar-mensagens-seguidos":
+					return ListFollowedMessages(payload);
+				return null;
 			}
+			
+			// um comando de erro
 			return new ErrorCommand(payload);
 		}
 	}
