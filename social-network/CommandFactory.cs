aaa998341ccc5b07fed433b55456e25755030f6c
Leandro Santiago <leandrosansilva@gmail.com>
2011-10-16 17:04:00 -0200

    Adicionada licença, autor, etc

--- social-network/CommandFactory.cs
+++ social-network/CommandFactory.cs
@@ -1,3 +1,20 @@
+/*
+  * This file is part of lssilva-social-network.
+  *
+  * lssilva-social-network is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * lssilva-social-network is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with lssilva-social-network.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 using System;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
